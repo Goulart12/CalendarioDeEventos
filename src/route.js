2e696476bf484d0b5e786b606beff221ca3f3c2f
Juarez Goulart <goulartgarcia12@gmail.com>
2021-10-22 22:18:30 -0300

    rotas

---
+++ src/route.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const route = express.Router();
+
+route.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+route.get('/login', (req, res) => {
+    res.render('login-page.ejs')
+})
+
+route.get('/create-account', (req, res) => {
+    res.render('create-account-page.ejs')
+})
+
+module.exports = route;
